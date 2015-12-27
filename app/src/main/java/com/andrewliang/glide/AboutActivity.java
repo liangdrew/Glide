6ddb1f1054b58c56cca3c97317d35e603f3b8034
liangdrew <liang.andrew@hotmail.com>
2015-12-27 16:02:06 -0500

    Delete AboutView, add 2 image files to drawable

--- app/src/main/java/com/andrewliang/glide/AboutActivity.java
+++ app/src/main/java/com/andrewliang/glide/AboutActivity.java
@@ -19,17 +19,6 @@ import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
-    private AboutView aboutView;
-    private Paint ABOUT_TEXT_PAINT = new Paint();
-
-    public Point getSize() {
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size;
-    }
-
     @Override
     protected void onResume() {
 
@@ -52,25 +41,7 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE); // requesting to turn the title OFF
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//
-//        ABOUT_TEXT_PAINT.setColor(Color.BLACK);
-//        ABOUT_TEXT_PAINT.setAntiAlias(true);
-//        ABOUT_TEXT_PAINT.setTextSize(getSize().x / 25);
-
-//        FrameLayout aboutLayout = new FrameLayout(this);
-//        aboutView = new AboutView(this, this);
-//        View aboutViewLayout = getLayoutInflater().inflate(R.layout.fragment_about, null);
-//        TextView aboutTitle = (TextView) findViewById(R.id.about_title);
-//        TextView aboutText = (TextView) findViewById(R.id.about_text);
-//        aboutTitle.getPaint().setFlags(Paint.ANTI_ALIAS_FLAG);
-//        aboutText.getPaint().setAntiAlias(true);
-
-//        aboutLayout.addView(aboutView);
-//        aboutLayout.addView(aboutViewLayout);
 
         setContentView(R.layout.activity_high_scores);
-//        setContentView(aboutLayout);
     }
 }
